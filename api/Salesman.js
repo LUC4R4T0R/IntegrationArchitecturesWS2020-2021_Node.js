f7a6879c4bb59c40c2ee68888292c6cc899941af
LUC4R4T0R <luc4r4t0r@gmail.com>
2020-12-01 18:13:37 +0100

    prerelease for test purposes

--- api/Salesman.js
+++ api/Salesman.js
@@ -1,11 +1,19 @@
-var Salesman = require('/models/Salesman');
+var Salesman = require('../models/Salesman');
+var managepersonal = require('../services/managepersonal');
 
 exports.list = function(req, res){
-
+    var db = req.app.get('db');
+    (async () => {
+        var result = await managepersonal.readSalesman(db);
+        res.send(result);
+    })();
 }
 
 exports.create = function(req, res){
-
+    var db = req.app.get('db');
+    let salesman = req.body;
+    managepersonal.createSalesman(db, salesman);
+    res.send('success');
 }
 
 exports.update = function(req, res){
@@ -13,7 +21,11 @@ exports.update = function(req, res){
 }
 
 exports.find = function(req, res){
-
+    var db = req.app.get('db');
+    (async () => {
+        var result = await managepersonal.readSalesman(db, undefined, {firstname: "test"});
+        res.send(result);
+    })();
 }
 
 exports.remove = function(req, res){
