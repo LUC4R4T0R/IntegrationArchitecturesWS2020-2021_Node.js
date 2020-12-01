031212d0797cfb5090eafb71e0bca7a3bff64191
Jonas <jonas@rbri.de>
2020-12-01 18:49:56 +0100

    fixed id bug of salesman read-request

--- services/managepersonal.js
+++ services/managepersonal.js
@@ -1,5 +1,5 @@
 //create Salesman
-exports.createSalesman = function (db, salesman) {
+exports.createSalesman = async function (db, salesman) {
     db.collection("salesman").insertOne(salesman, function (err, res) {
         if (err) throw err;
         console.log("1 Salesman inserted");
@@ -13,33 +13,32 @@ exports.readSalesman = async function (db, salesmanId, query) {
     } else if (salesmanId === undefined) {
         return db.collection("salesman").find(query).toArray();
     } else if (query === undefined) {
-        var test = await db.collection("salesman").findOne({id: salesmanId});
-        console.log(test);
-        return test;
+        test = db.collection("salesman").findOne({id: parseInt(salesmanId)});
+        return test[0];
     } else {
         //throw exception
     }
 }
 
 //update Salesman
-exports.updateSalesman = function (db, salesman) {
-    var newvalues = {$set: {firstname: salesman.firstname, address: salesman.lastname}};
-    db.collection("salesman").updateOne({id: salesman.id}, newvalues, function (err, res) {
+exports.updateSalesman = async function (db, salesman) {
+    var newvalues = {$set: {firstname: salesman.firstname, lastname: salesman.lastname}};
+    db.collection("salesman").updateOne({id: parseInt(salesman.id)}, newvalues, function (err, res) {
         if (err) throw err;
         console.log("1 Salesman updated");
     });
 }
 
 //delete Salesman
-exports.deleteSalesman = function (db, id) {
-    db.collection("salesman").deleteOne({id: id}, function (err, obj) {
+exports.deleteSalesman = async function (db, id) {
+    db.collection("salesman").deleteOne({id: parseInt(id)}, function (err, obj) {
         if (err) throw err;
         console.log("1 Salesman deleted");
     });
 }
 
 //create Evaluationrecord
-exports.createEvaluationrecord = function (db, id, evaluationrecord) {
+exports.createEvaluationrecord = async function (db, id, evaluationrecord) {
     db.collection("records").insertOne({id: id, evaluationrecord: evaluationrecord}, function (err, res) {
         if (err) throw err;
         console.log("1 Evaluationrecord inserted");
@@ -47,16 +46,12 @@ exports.createEvaluationrecord = function (db, id, evaluationrecord) {
 }
 
 //read Evaluationrecord
-exports.readEvaluationrecord = function (db, year, id) {
+exports.readEvaluationrecord = async function (db, year, id) {
     if (year === undefined && id === undefined) {
-        db.collection("records").findOne({}).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
+        return db.collection("records").findOne({}).toArray(function (err, result) {
         });
     } else if (year !== undefined && id !== undefined) {
-        db.collection("records").findOne({id: id, year: evaluationrecord.year}, function (err, result) {
-            if (err) throw err;
-            console.log(result);
+        return db.collection("records").findOne({id: id, year: evaluationrecord.year}, function (err, result) {
         });
     } else {
         //throw exception
@@ -64,7 +59,7 @@ exports.readEvaluationrecord = function (db, year, id) {
 }
 
 //delete Evaluationrecord
-exports.deleteEvaluationrecord = function (db, year, id) {
+exports.deleteEvaluationrecord = async function (db, year, id) {
     db.collection("salesman").deleteOne({id: id, year: evaluationrecord.year}, function (err, obj) {
         if (err) throw err;
         console.log("1 Evaluationrecord deleted");
@@ -73,21 +68,42 @@ exports.deleteEvaluationrecord = function (db, year, id) {
 
 
 //create Evaluationrecordentry
-exports.createEvaluationrecordentry = function (db, id, year,evaluationrecordentry) {
+exports.createEvaluationrecordentry = async function (db, id, year, evaluationrecordentry) {
+    var evaluationrecord;
+    db.collection("records").findOne({id: id, year: evaluationrecord.year}, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        evaluationrecord = result;
+    });
 
+    evaluationrecord.entries[evaluationrecord.entries.length] = evaluationrecordentry;
 }
 
 //read Evaluationrecordentry
-exports.readEvaluationrecordentry = function (db, year, id) {
+exports.readEvaluationrecordentry = async function (db, year, id, name) {
 
 }
 
 //update Evaluationrecordentry
-exports.updateEvaluationrecordentry = function (db, salesman) {
+exports.updateEvaluationrecordentry = async function (db, evaluationrecordentry, id, year) {
+    var evaluationrecord;
+    db.collection("records").findOne({id: id, year: evaluationrecord.year}, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        evaluationrecord = result;
+    });
+
+    var entries = evaluationrecord.entries;
+    for (let i = 0; i < entries.length; i++) {
+        if (entries[i].name == name) {
+            entries[i] = evaluationrecordentry;
+        }
+    }
+
 
 }
 
 //delete Evaluationrecordentry
-exports.deleteEvaluationrecordentry = function (db, year, id) {
+exports.deleteEvaluationrecordentry = async function (db, year, id) {
 
 }
