b81f18a71570cc6434efa99e374538735b9b575b
LUC4R4T0R <luc4r4t0r@gmail.com>
2020-12-01 15:12:54 +0100

    planned basic Objects

---
+++ models/EvaluationRecord.js
@@ -0,0 +1,7 @@
+class EvaluationRecord{
+    constructor(year, entries) {
+        this.year = year;
+        this.entries = entries;
+    }
+}
+module.exports = EvaluationRecord;
---
+++ models/EvaluationRecordEntry.js
@@ -0,0 +1,9 @@
+class EvaluationRecordEntry{
+    constructor(name, target, actual) {
+        this.name = name;
+        this.target = target;
+        this.actual = actual;
+    }
+}
+
+module.exports = EvaluationRecordEntry;
