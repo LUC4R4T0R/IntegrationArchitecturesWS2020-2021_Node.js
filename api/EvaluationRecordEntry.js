e811013dd8ecf40cf9e0e9331e482df77c7e3c63
LUC4R4T0R <luc4r4t0r@gmail.com>
2020-12-03 10:33:23 +0100

    basic Implementation of EvaluationRecordEntry API-Controller

--- api/EvaluationRecordEntry.js
+++ api/EvaluationRecordEntry.js
@@ -1,21 +1,40 @@
 var EvaluationRecordEntry = require('../models/EvaluationRecordEntry');
+var managepersonal = require('../services/managepersonal');
 
 exports.list = function(req, res){
-
+    var db = req.app.get('db');
+    (async () => {
+        var result = await managepersonal.readEvaluationRecordentry(db, req.params.year, req.params.id);
+        res.send(result);
+    })();
 }
 
 exports.create = function(req, res){
-
+    var db = req.app.get('db');
+    let evaluationRecordEntry = req.body;
+    managepersonal.createEvaluationRecordentry(db, req.params.id, req.params.year, evaluationRecordEntry).then(() => {
+        res.send('success');
+    });
 }
 
 exports.update = function(req, res){
-
+    var db = req.app.get('db');
+    let evaluationRecordEntry = req.body;
+    managepersonal.updateEvaluationRecordentry(db, evaluationRecordEntry, req.params.id, req.params.year);
+    res.send('success');
 }
 
 exports.find = function(req, res){
-
+    var db = req.app.get('db');
+    (async () => {
+        var result = await managepersonal.readEvaluationRecordentry(db, req.params.year, req.params.id, req.params.name);
+        res.send(result);
+    })();
 }
 
 exports.remove = function(req, res){
-
+    var db = req.app.get('db');
+    managepersonal.deleteEvaluationRecordentry(db, req.params.year, req.params.id).then(() => {
+        res.send('success');
+    });
 }
