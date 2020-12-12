b08fb82fb23ce2eb949a7e71339aa6a2cecb945a
Jonas <jonas@rbri.de>
2020-12-12 11:27:23 +0100

    fixed promise error

--- api/Salesman.js
+++ api/Salesman.js
@@ -5,15 +5,15 @@ let salesman_service = require('../services/Salesman');
     let db = req.app.get('db');
     let salesman = req.body;
     salesman_service.createSalesman(db, salesman)
-        .then(res.send('success'))
-        .catch(res.status(400).send("fail"));
+        .then(() => res.send('success'))
+        .catch(() => res.status(400).send("fail"));
 }*/
 
 exports.list = function(req, res){
     let db = req.app.get('db');
     salesman_service.readSalesman(db)
         .then(result => res.send(result))
-        .catch(res.status(400).send("fail"));
+        .catch(() => res.status(400).send("fail"));
 }
 
 exports.find = function(req, res){
@@ -27,14 +27,14 @@ exports.find = function(req, res){
     let db = req.app.get('db');
         let salesman = req.body;
         salesman_service.updateSalesman(db, salesman)
-            .then(res.send('success'))
+            .then(() => res.send('success'))
             .catch(() => res.status(400).send("fail"));
 }*/
 
 /*exports.remove = function(req, res) {
     let db = req.app.get('db');
     salesman_service.deleteSalesman(db, req.params.id)
-        .then(res.send('success'))
-        .catch(res.status(400).send("fail"));
+        .then(() => res.send('success'))
+        .catch(() => res.status(400).send("fail"));
 
 }*/
