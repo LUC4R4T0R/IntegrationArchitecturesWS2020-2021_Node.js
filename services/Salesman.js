35420384134a1f4302ea83468962e915c9cd5e2c
Jonas <jonas@rbri.de>
2020-12-21 11:34:49 +0100

    added endpoint to search for a name and get every salesman with this name as first or lastname

--- services/Salesman.js
+++ services/Salesman.js
@@ -20,11 +20,11 @@ let MissingElementError = require('../custom_errors/MissingElementError');
 };*/
 
 //read Salesman
-exports.readSalesman = async function (db, id, query) {
+exports.readSalesman = async function (db, id, queryString) {
     if (db === undefined) {
         throw new MissingElementError("MissingElementError: At least one of the required parameters is undefined!");
     } else {
-        if (id === undefined && query === undefined) {
+        if (id === undefined && queryString === undefined) {
             let test = await db.collection("salesman").find({}).toArray();
             if (test.length === 0) {
                 throw new NoElementFoundError("NoElementFoundError: In the given Database exists no Salesman");
@@ -33,14 +33,15 @@ exports.readSalesman = async function (db, id, query) {
                 return test;
             }
         } else if (id === undefined) {
-            let test = await db.collection("salesman").find(query).toArray();
-            if (test.length === 0) {
+            let test1 = await db.collection("salesman").find({firstname: queryString}).toArray();
+            let test2 = await db.collection("salesman").find({lastname: queryString}).toArray();
+            if (test1.length === 0 && test2.length === 0) {
                 throw new NoElementFoundError("NoElementFoundError: In the given Database exists no Salesman with the given first-/lastname!");
             } else {
                 //return all elements of the collection with have a specific firstname and/or lastname
-                return test;
+                return test1.push(...test2);
             }
-        } else if (query === undefined) {
+        } else if (queryString === undefined) {
             if (!id.match(/^[\d]+$/g)){
                 throw new BadInputError("BadInputError: The id must be a numbers (example input: 1234)!");
             }
