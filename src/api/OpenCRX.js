bf06c71c8fb0c8147c43580e6798c790e9da5ccc
Jonas <jonas@rbri.de>
2020-12-28 14:35:09 +0100

    created opencrx api

---
+++ src/api/OpenCRX.js
@@ -0,0 +1,8 @@
+let opencrx_service = require('../connectors/OpenCRX');
+let auth_service = require('../services/Authentication');
+
+exports.do = function (req, res) {
+}
+
+exports.something = function (req, res) {
+}
